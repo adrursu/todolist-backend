4534459faa4beb319ea8e85aef95b14745ca2efe
adrursu <adrianursu@gmail.com>
2024-10-09 15:03:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: {
-        host: 'todolistdbau.mysql.database.azure.com', // This matches the name of the MySQL service in Docker Compose
+        host: 'http://todolistdbau.mysql.database.azure.com', // This matches the name of the MySQL service in Docker Compose
         user: 'todolistadmin',
         password: 'welcome@123',
         database: 'todo_db'
